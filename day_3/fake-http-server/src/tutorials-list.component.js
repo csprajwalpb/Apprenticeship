aa8728386d7be63be1c404eb1b633d1959fc6d81
Prajwal P B <prajwalpb8055@gmail.com>
2025-07-31 18:48:16 +0530

    created useMemo, useCallback, fakeApi's

---
+++ day_3/fake-http-server/src/tutorials-list.component.js
@@ -0,0 +1,65 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import tutorialsService from './services/tutorials.service';
+
+const TutorialsListComponent=()=>{
+
+    const [tutorials,setTutorials]=useState([]);
+    const [currentTutorial,setCurrentTutorial] =useState({});
+  useEffect(()=>{
+    retrieveTutorials();
+  },[]);  
+
+ const retrieveTutorials =(e)=>{   
+    tutorialsService.getAll()
+      .then((response)=>{
+        console.log(response);
+        setTutorials(response.data);
+      })
+      .catch((error) => {
+      console.error("Error fetching tutorials:", error.message);
+    });
+ }
+  
+  
+ const showTutorialDescription=(tutorial,index)=>{
+
+    console.log(tutorial,index);
+    setCurrentTutorial(tutorial);
+
+ }
+  
+ 
+return(
+ <>
+    <h1>Tutorials Component List </h1>
+    {tutorials && tutorials.map((tutorial, index) => (
+  <p key={tutorial.id} onClick={() => showTutorialDescription(tutorial, index)}>
+    {tutorial.title} - {tutorial.description}
+  </p>
+))}
+
+
+    <br/>
+    <p>Click on any tutorial to get description below</p>
+
+      <h2>Selected Tutorial</h2>
+      {currentTutorial.title}<br/>
+      {currentTutorial.description}<br/>
+      {currentTutorial.published? "It's Published":"It's Not Published"}<br/>
+      {currentTutorial.id && (
+  <Link to={"/tutorials/" + encodeURIComponent(currentTutorial.id)}>Edit</Link>
+)}
+
+    
+	
+ </>
+
+);
+
+
+}
+
+
+
+export default TutorialsListComponent;
